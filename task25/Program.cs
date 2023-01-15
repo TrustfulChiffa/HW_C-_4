4f3ff5f7d59a28163cf178eac4c309b55d9ae0fa
Chiffa <babiy99021422@gmail.com>
2023-01-15 21:34:38 +0300

    Сделал задание 25

---
+++ task25/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int sexponentiation(int number, int degree)
+{
+    int c = 1;
+
+    for (int i = 0; i < degree; i++)
+    {
+        c *= number;
+    }
+    return c;
+}
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень, в которую необходимо возвести число: ");
+int deg = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+int res = sexponentiation(num, deg);
+
+Console.WriteLine($"{num}, {deg} -> {res}");
