54ddd49f84d3c77a766d455ea48864c41188069d
Chiffa <babiy99021422@gmail.com>
2023-01-17 22:50:30 +0300

    Сделал задание 27

---
+++ task27/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int length(int num)
+{
+    int i = 0;
+    for (; num > 0; num /= 10)
+    {
+        i++;
+    }
+    return i;
+}
+
+int ProductOfDigits(int len, int num)
+{
+    int product = 0;
+
+    for (; len > 0; len -= 1)
+    {
+        product += (num % 10);
+        num /= 10;
+    }
+
+    return product;
+}
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+int L = length(N);
+
+int P = (ProductOfDigits(L, N));
+
+Console.WriteLine($"{N} -> {P}");
